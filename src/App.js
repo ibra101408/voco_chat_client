6b34c33d74b922f18726d3c2da5b89aa308d32bd
User Name <danikvod@gmail.com>
2022-03-09 21:02:53 +0200

    front and backend side working version

--- src/App.js
+++ src/App.js
@@ -1,31 +1,41 @@
 import './App.css';
 import io from 'socket.io-client';
 import {useState} from "react";
+import Chat from "./Chat";
 
 const socket = io.connect('http://localhost:3001');
 
 function App() {
     const [username, setUsername] = useState("");
    // const [room, setRoom] = useState("");
+    const [showChat, setShowChat] = useState(false);
+
     const room = "voco";
+
     const joinRoom = () =>{
         if (username !== ""){
             socket.emit("join_room", room);
+            setShowChat(true);
         }
     };
 
   return (
     <div className="App">
-        <h3> Join a Chat </h3>
-        <input
-            type="text"
-            placeholder="Username"
-        onChange={(event) => {
-            setUsername(event.target.value);
-        }}/>
-        <button onClick={joinRoom}> Join </button>
-
-
+        {!showChat ? (
+        <div className="joinChatContainer">
+            <h3> Join a Chat </h3>
+            <input
+                type="text"
+                placeholder="Username"
+            onChange={(event) => {
+                setUsername(event.target.value);
+            }}/>
+            <button onClick={joinRoom}> Join </button>
+        </div>
+            )
+            : (
+            <Chat socket={socket} username={username} room={room}/>
+            )};
 
     </div>
   );
