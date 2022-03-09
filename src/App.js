701200583809ae4b7e4f92c9ffa8348a82849951
User Name <danikvod@gmail.com>
2022-03-09 13:29:56 +0200

    front and backend side

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
 import './App.css';
+import io from 'socket.io-client';
+import {useState} from "react";
+
+const socket = io.connect('http://localhost:3001');
 
 function App() {
+    const [username, setUsername] = useState("");
+   // const [room, setRoom] = useState("");
+    const room = "voco";
+    const joinRoom = () =>{
+        if (username !== ""){
+            socket.emit("join_room", room);
+        }
+    };
+
   return (
-    <div className="App">heee</div>
+    <div className="App">
+        <h3> Join a Chat </h3>
+        <input
+            type="text"
+            placeholder="Username"
+        onChange={(event) => {
+            setUsername(event.target.value);
+        }}/>
+        <button onClick={joinRoom}> Join </button>
+
+
+
+    </div>
   );
 }
 
